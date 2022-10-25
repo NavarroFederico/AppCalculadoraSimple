cd81818395dd697c4dc94e52e8fc71da1479d856
Federico Navarro <navarrofederico14@gmail.com>
2022-10-25 10:56:58 -0300

    correccion de botones . color de botones

--- app/src/main/java/com/example/calculadorasimple/ui/MainActivity.kt
+++ app/src/main/java/com/example/calculadorasimple/ui/MainActivity.kt
@@ -3,12 +3,43 @@ package com.example.calculadorasimple.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calculadorasimple.R
+import com.example.calculadorasimple.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
+    lateinit var binding: ActivityMainBinding
+    private var operacionSeleccionada : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlus.setOnClickListener{
+            operacionSeleccionada = 1
+            actualizarBotones()
+        }
+
+    }
+
+    private fun actualizarBotones() {
+        when(operacionSeleccionada){
+            0->{
+
+            }
+            1->{
+
+            }
+            2->{
+
+            }
+            3->{
+
+            }
+            4->{
+
+            }
+            else->Unit
+
+
+        }
     }
 }
